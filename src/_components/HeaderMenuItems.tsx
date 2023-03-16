e3bfeb8fdb946ba253b2e188c9cca60eafa1f7a0
adb-sh <git@adb.sh>
2023-03-16 15:50:13 +0100

    Debug header

--- src/_components/HeaderMenuItems.tsx
+++ src/_components/HeaderMenuItems.tsx
@@ -17,11 +17,11 @@ export default ({ search, ...props }) => (
         const subpages = search.pages(`header(${page.data.slug})`, "date=desc");
         if (subpages.length > 0) {
           return (
-            <li class="category dropdown dropdown-side">
+            <li class="category dropdown">
               <div class="btn btn-outline-dark btn-shadow m-2 dropdown-label" tabindex="0">
                 {page.data.title}
               </div>
-              <div class="d-block position-relative dropdown-menu btn-outline-dark p-0 mx-2">
+              <div class="d-block dropdown-menu btn-outline-dark p-0 mx-2">
                 {subpages.map((subpage) => <a class="dropdown-item" href={subpage.data.url}>{subpage.data.title}</a>)}
               </div>
             </li>
