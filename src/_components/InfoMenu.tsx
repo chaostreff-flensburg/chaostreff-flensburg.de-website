1a72a13e1bee7456a4d426c0eddbb5692729e945
scammo <sbrinkmann@54gradsoftware.de>
2023-05-19 21:01:17 +0200

    🧹 Links in der Navigation

--- src/_components/InfoMenu.tsx
+++ src/_components/InfoMenu.tsx
@@ -2,12 +2,13 @@ export default () => (
   <div class="infoMenu sticky-top">
     <b><strong>Kontakt und Social Media:</strong></b>
     <ul class="d-flex flex-column">
-      <li><a class="btn btn-outline-dark btn-shadow m-2" href="mailto:mail@chaostreff-flensburg.de">mail@chaostreff-flensburg.de</a></li>
-      <li><a class="btn btn-outline-dark btn-shadow m-2" href="https://twitter.com/chaos_fl">Twitter</a></li>
-      <li><a class="btn btn-outline-dark btn-shadow m-2" href="https://www.facebook.com/chaos.flensburg">Facebook</a></li>
-      <li><a class="btn btn-outline-dark btn-shadow m-2" href="https://www.instagram.com/chaos_fl/">Instagram</a></li>
-      <li><a class="btn btn-outline-dark btn-shadow m-2" href="https://www.flickr.com/photos/157585171@N05/">Flickr</a></li>
-      <li><a class="btn btn-outline-dark btn-shadow m-2" href="https://github.com/chaostreff-flensburg">Github</a></li>
+      <li><a class="btn btn-outline-dark btn-shadow my-2" href="mailto:mail@chaostreff-flensburg.de">mail@chaostreff-flensburg.de</a></li>
+      <li><a class="btn btn-outline-dark btn-shadow my-2" href="https://chaos.social/@chaos_fl">Mastodon @chaos_fl@chaos.social</a></li>
+      <li><a class="btn btn-outline-dark btn-shadow my-2" href="https://twitter.com/chaos_fl">Twitter</a></li>
+      <li><a class="btn btn-outline-dark btn-shadow my-2" href="https://www.instagram.com/chaos_fl/">Instagram</a></li>
+      <li><a class="btn btn-outline-dark btn-shadow my-2" href="https://www.flickr.com/photos/157585171@N05/">Flickr</a></li>
+      <li><a class="btn btn-outline-dark btn-shadow my-2" href="https://github.com/chaostreff-flensburg">Github</a></li>
+      <li><a class="btn btn-outline-dark btn-shadow my-2" href="/feed.rss">RSS</a></li>
     </ul>
     <b><strong>Rechtliches:</strong></b>
     <ul class="d-flex flex-column">
